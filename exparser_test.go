7bf920504200151d8fee876f7bc6f2fd716d5487
chernykovv <chernykovv@gmail.com>
2021-12-14 08:19:55 -0800

    JSON query library in Golang for Test

---
+++ exparser_test.go
@@ -0,0 +1,102 @@
+package jsonql
+
+import (
+	"testing"
+)
+
+func TestTokenize(t *testing.T) {
+
+	var pass = []struct {
+		in string
+		ex []string
+	}{
+		{"-1 + 2", []string{"-1", "+", "2"}},
+		{"-(1+2)", []string{"-", "(", "1", "+", "2", ")"}},
+		{"+1+2", []string{"+1", "+", "2"}},
+		{"1+2+(3*4)", []string{"1", "+", "2", "+", "(", "3", "*", "4", ")"}},
+		{"1+2+(3*4)^34", []string{"1", "+", "2", "+", "(", "3", "*", "4", ")", "^", "34"}},
+		{"'123  456' 789", []string{"'123  456'", "789"}},
+		{`123 "456  789"`, []string{"123", "\"456  789\""}},
+		{`123 "456  '''789"`, []string{"123", "\"456  '''789\""}},
+		{`2*x+y is null`, []string{"2", "*", "x", "+", "y", "is", "null"}},
+	}
+	var fail = []struct {
+		in string
+		ex []string
+	}{}
+	parser := &Parser{
+		Operators: sqlOperators,
+	}
+	for _, v := range pass {
+		tokens := parser.Tokenize(v.in)
+		if !CompareSlices(tokens, v.ex) {
+			t.Error("Expected:", v.ex, len(v.ex), "actual:", tokens, len(tokens))
+		}
+	}
+	for _, v := range fail {
+		tokens := parser.Tokenize(v.in)
+		if CompareSlices(tokens, v.ex) {
+			t.Error("Expected:", v.ex, "actual:", tokens)
+		}
+	}
+}
+
+func TestTokenizeSql(t *testing.T) {
+	var pass = []struct {
+		in string
+		ex []string
+	}{
+		{"true AND false", []string{"true", "AND", "false"}},
+	}
+	var fail = []struct {
+		in string
+		ex []string
+	}{}
+	parser := &Parser{
+		Operators: sqlOperators,
+	}
+	for _, v := range pass {
+		tokens := parser.Tokenize(v.in)
+		if !CompareSlices(tokens, v.ex) {
+			t.Error("Expected:", v.ex, "actual:", tokens)
+		}
+	}
+	for range fail {
+
+	}
+}
+
+func TestRPN(t *testing.T) {
+	var pass = []struct {
+		in string
+		ex string
+	}{
+		{"true && false", "false"},
+		{"true && true", "true"},
+		{"false && false", "false"},
+		{"true || true", "true"},
+		{"true || (true && false)", "true"},
+		{"true && (false && true)", "false"},
+		{"(true && false) || (false || true)", "true"},
+	}
+	var fail = []struct {
+		in string
+		ex []string
+	}{}
+	parser := &Parser{
+		Operators: sqlOperators,
+	}
+
+	for _, v := range pass {
+		result, err := parser.Calculate(v.in)
+		if err != nil {
+			t.Error(err)
+		}
+		if result != v.ex {
+			t.Error(v.in, "Expected:", v.ex, "actual:", result)
+		}
+	}
+	for range fail {
+
+	}
+}
---
+++ jsonql_test.go
@@ -0,0 +1,71 @@
+package jsonql
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+
+	jsonString := `
+[
+  {
+    "name": "elgs",
+    "gender": "m",
+    "skills": [
+      "Golang",
+      "Java",
+      "C"
+    ]
+  },
+  {
+    "name": "enny",
+    "gender": "f",
+    "skills": [
+      "IC",
+      "Electric design",
+      "Verification"
+    ]
+  },
+  {
+    "name": "sam",
+    "gender": "m",
+    "skills": [
+      "Eating",
+      "Sleeping",
+      "Crawling"
+    ]
+  }
+]
+`
+	parser, err := NewStringQuery(jsonString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	var pass = []struct {
+		in string
+	}{
+		{"name='elgs'"},
+		{"gender='f'"},
+		{"skills.[1]='Sleeping'"},
+		{"skills contains 'Verification'"},
+	}
+	var fail = []struct {
+		in string
+		ex interface{}
+	}{}
+	for _, v := range pass {
+		result, err := parser.Query(v.in)
+		if err != nil {
+			t.Error(v.in, err)
+		}
+		fmt.Println(v.in, result)
+		//		if v.ex != result {
+		//			t.Error("Expected:", v.ex, "actual:", result)
+		//		}
+	}
+	for range fail {
+
+	}
+}
